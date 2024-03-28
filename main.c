76725df6f179ede456597d01328a692e80eae704
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 22:28:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 stack_t *head = NULL;
+char **op_toks = NULL;
 
 /**
  * main - entry point
@@ -19,7 +20,28 @@ int main(int argc, char *argv[])
 	free_nodes();
 	return (0);
 }
+/**
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
+{
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
+}
 /**
  * create_node - Creates a node.
  * @n: Number to go inside the node.
