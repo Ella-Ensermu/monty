a5f30559e6dabd49dddd06d9edfe33d0f3ad0d4e
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 16:06:55 +0300

    Update push.c

--- push.c
+++ push.c
@@ -1,26 +1,26 @@
 #include "monty.h"
 /**
- * f_push - add node to the stack
+ * func_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void func_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int num, j = 0, flag = 0;
 
-	if (bus.arg)
+	if (data.arg)
 	{
-		if (bus.arg[0] == '-')
+		if (data.arg[0] == '-')
 			j++;
-		for (; bus.arg[j] != '\0'; j++)
+		for (; data.arg[j] != '\0'; j++)
 		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+			if (data.arg[j] > 57 || bus.arg[j] < 48)
 				flag = 1; }
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
+			fclose(data.file);
+			free(data.content);
 			free_stack(*head);
 			exit(EXIT_FAILURE); }}
 	else
@@ -29,8 +29,8 @@ void f_push(stack_t **head, unsigned int counter)
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
+	num = atoi(data.arg);
+	if (data.lifo == 0)
 		addnode(head, n);
 	else
 		addqueue(head, n);
