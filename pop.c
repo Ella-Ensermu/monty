4d34b809c5040edc6c930ff5197c07b83891e751
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 13:14:43 +0300

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,20 +1,20 @@
 #include "monty.h"
 /**
  * f_pop - prints the top
- * @head: stack head
+ * @head: node head
  * @counter: line_number
  * Return: no return
 */
-void f_pop(stack_t **head, unsigned int counter)
+void f_pop(node_t **head, unsigned int counter)
 {
-	stack_t *h;
+	node_t *h;
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't pop an empty node\n", counter);
+		fclose(data.file);
+		free(data.content);
+		free_node(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
