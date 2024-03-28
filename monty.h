751ee36018941d8627829c749261b26bbb350926
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 22:17:04 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,83 +1,152 @@
-#ifndef MONTY_H
-#define MONTY_H
+#include "monty.h"
+/**
+ * print_str - Prints a string.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *tmp;
 
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <ctype.h>
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
 
 /**
- * struct data_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifo: flag change stack <-> queue
- * Description: carries values through the program
+ * rotl - Rotates the first node of the stack to the bottom.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
  */
-typedef struct data_s
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+/**
+ * print_char - Prints the Ascii value.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_number);
+
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+	printf("%c\n", ascii);
+}
+/**
+ * rotr - Rotates the last node of the stack to the top.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
+/**
+* struct instruction_s - opcode and its function
+* @opcode: the opcode
+* @f: function to handle the opcode
+*
+* Description: opcode and its function
+* for stack, queues, LIFO, FIFO
+*/
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+* struct bus_s - variables -args, file, line content
+* @arg: value
+* @file: pointer to monty file
+* @content: line content
+* @lifi: flag change stack <-> queue
+*
+* Description: carries values through the program
+*/
+typedef struct bus_s
 {
 	char *arg;
 	FILE *file;
 	char *content;
-	int lifo;
-}  data_t;
-extern data_t bus;
-
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @num: integer
- * @preve: points to the previous element of the stack (or queue)
- * @nex: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct stack_s
-{
-	int num;
-	struct stack_s *preve;
-	struct stack_s *nex;
-} stack_t;
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @func: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*func)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
 
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-void func_push(stack_t **head, unsigned int number);
-void func_pall(stack_t **head, unsigned int number);
-void func_pint(stack_t **head, unsigned int number);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-void func_pop(stack_t **head, unsigned int counter);
-void func_swap(stack_t **head, unsigned int counter);
-void func_add(stack_t **head, unsigned int counter);
-void func_nop(stack_t **head, unsigned int counter);
-void func_sub(stack_t **head, unsigned int counter);
-void func_div(stack_t **head, unsigned int counter);
-void func_mul(stack_t **head, unsigned int counter);
-void func_mod(stack_t **head, unsigned int counter);
-void func_pchar(stack_t **head, unsigned int counter);
-void func_pstr(stack_t **head, unsigned int counter);
-void func_rotl(stack_t **head, unsigned int counter);
-void func_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
-void func_queue(stack_t **head, unsigned int counter);
-void func_stack(stack_t **head, unsigned int counter);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
+
+
 #endif
