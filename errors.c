1c8a6a99223a1afede5005a7c8dd628e0dfb41f0
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 22:20:48 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,4 +1,26 @@
 #include "monty.h"
+/**
+ * malloc_error - Prints malloc error messages.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * f_open_error - Prints file opening error messages w/ file name.
+ * @filename: Name of file failed to open
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int f_open_error(char *filename)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	return (EXIT_FAILURE);
+}
 
 /**
  * err - Prints appropiate error messages determined by their error code.
@@ -45,6 +67,19 @@ void err(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+/**
+ * unknown_op_error - Prints unknown instruction error messages.
+ * @opcode: Opcode where error occurred.
+ * @line_number: Line number in Monty bytecodes file where error occured.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int unknown_op_error(char *opcode, unsigned int line_number)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n",
+		line_number, opcode);
+	return (EXIT_FAILURE);
+}
 
 /**
  * more_err - handles errors.
@@ -86,7 +121,17 @@ void more_err(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
-
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
+}
 /**
  * string_err - handles errors.
  * @error_code: The error codes are the following:
