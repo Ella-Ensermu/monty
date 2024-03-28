d4c59c78bc59bae4dc5fb01b3d9e41043fa68f81
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 22:23:34 +0300

    Update file_tools.c

--- file_tools.c
+++ file_tools.c
@@ -1,4 +1,21 @@
 #include "monty.h"
+int short_stack_error(unsigned int line_number, char *op);
+int div_error(unsigned int line_number);
+int pop_error(unsigned int line_number);
+int div_error(unsigned int line_number);
+int pchar_error(unsigned int line_number, char *message);
+
+/**
+ * pop_error - Prints pop error messages for empty stacks.
+ * @line_number: Line number in script where error occured.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int pop_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+	return (EXIT_FAILURE);
+}
 
 /**
  * open_file - opens a file
@@ -36,7 +53,17 @@ void read_file(FILE *fd)
 	}
 	free(buffer);
 }
-
+/**
+ * pint_error - Prints pint error messages for empty stacks.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int pint_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	return (EXIT_FAILURE);
+}
 
 /**
  * parse_line - Separates each line into tokens to determine
@@ -68,6 +95,31 @@ int parse_line(char *buffer, int line_number, int format)
 
 	find_func(opcode, value, line_number, format);
 	return (format);
+}
+/**
+ * short_stack_error - Prints monty math function error messages
+ *                     for stacks/queues smaller than two nodes.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ * @op: Operation where the error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int short_stack_error(unsigned int line_number, char *op)
+{
+	fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * div_error - Prints division error messages for division by 0.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int div_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: division by zero\n", line_number);
+	return (EXIT_FAILURE);
 }
 
 /**
@@ -118,7 +170,19 @@ void find_func(char *opcode, char *value, int ln, int format)
 		err(3, ln, opcode);
 }
 
-
+/**
+ * pchar_error - Prints pchar error messages for empty stacks
+ *               empty stacks and non-character values.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ * @message: The corresponding error message to print.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int pchar_error(unsigned int line_number, char *message)
+{
+	fprintf(stderr, "L%u: can't pchar, %s\n", line_number, message);
+	return (EXIT_FAILURE);
+}
 /**
  * call_fun - Calls the required function.
  * @func: Pointer to the function that is about to be called.
