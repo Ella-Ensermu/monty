99f9e08da32981838063faced576e6393e7bbf4e
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-28 13:28:02 +0300

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 /**
- * insertNode - insert node at the beginning of the stack
- * @head: head of the stack
+ * insertNode - insert node at the beginning of the node
+ * @head: head of the node
  * @value: new_value
  * Return: no return
 */
-void insertNode(stack_t **head, int value)
+void insertNode(node_t **head, int value)
 {
 
-	stack_t *newNode, *temp;
+	node_t *newNode, *temp;
 
 	temp = *head;
-	newNode = malloc(sizeof(stack_t));
+	newNode = malloc(sizeof(node_t));
 	if (newNode == NULL)
 	{ printf("Error\n");
 		exit(0); }
